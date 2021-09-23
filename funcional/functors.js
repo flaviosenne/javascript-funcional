fade15195e9fe36149eb59e00daf54dd107e6f7d
joao flavio <flaviosenne123@gmail.com>
2021-09-23 09:29:34 -0300

    functors

---
+++ funcional/functors.js
@@ -0,0 +1,36 @@
+// functors são objetos que implementam a função MAP
+// que também é im "wrapper" de valor
+
+// ARRAY é um excemplo de FUNCTORS
+const nums = [ 1,2,3,4,5,6]
+
+const novosNums = nums
+.map(el => el+10)
+.map(el => el * 2)
+
+console.log(nums, novosNums)
+
+function TipoSeguro(valor){
+    return {
+        valor,  
+        invalid(){
+            return this.valor === null || this.valor === undefined
+        },
+        map(fn){
+            if(this.invalid()){
+                return TipoSeguro(null)
+            }else{
+                const novoValor = fn(this.valor)
+                return TipoSeguro(novoValor)
+            }
+        }
+    }
+}
+const original = 'Esse é um texto'
+const alterado = TipoSeguro(original)
+.map(t => t.toUpperCase())
+.map(t => `${t}!!!`)
+.map(t => t.split('').join(' '))
+
+
+console.log(original, alterado.valor)
